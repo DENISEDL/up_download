f6cf0a5d0a80c216ca7a41e38934a4351ed807ff
DENISE.DL <denise.dl@hotmail.it>
2024-04-23 17:13:28 +0200

    fatto

---
+++ src/main/java/com/example/up_down/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.up_down.repositories;
+
+import com.example.up_down.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
